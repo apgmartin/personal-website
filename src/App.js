a36c74e043765c727eae8d51675599e09b238bda
Ali Martin <alistairmartin@live.com>
2018-06-29 14:25:01 +0100

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import "./App.css";
+import NavBar from './components/nav-bar/NavBar';
 
 class App extends Component{
   render(){
     return(
-      <div className="App container">
+      <div className="App">
+        <NavBar/>
         <h1>Ali's Website is amazing!</h1>
       </div>
     );
