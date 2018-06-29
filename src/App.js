f0abd240cfe29210ac54b094076f73835fabec8b
Ali Martin <alistairmartin@live.com>
2018-06-29 13:43:10 +0100

    Project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App container">
+        <h1>Ali's Website is amazing!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
