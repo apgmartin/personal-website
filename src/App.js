6e7345d6f8209cb3f426bedd39302b87ffc2ea7d
Ali Martin <alistairmartin@live.com>
2018-06-29 17:56:09 +0100

    Created four full page holders with arrows

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import "./App.css";
 import NavBar from './components/nav-bar/NavBar';
+import FullPageContent from './components/full-page-content/FullPageContent';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
         <NavBar/>
-        <h1>Ali's Website is amazing!</h1>
+        <FullPageContent pageClass='page-1' displayArrow={true}/>
+        <FullPageContent pageClass='page-2' displayArrow={true}/>
+        <FullPageContent pageClass='page-3' displayArrow={true}/>
+        <FullPageContent pageClass='page-4' displayArrow={false}/>
       </div>
     );
   }
